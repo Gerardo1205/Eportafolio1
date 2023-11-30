6155f81da2e7d967e06f841d340d34a99e131462
Gerardo Magana <ivan.magana98@uabc.edu.mx>
2023-11-29 21:50:33 -0800

    Datos

---
+++ Eportafolio1/Controllers/AcercasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio1.Models;
+
+namespace Eportafolio1.Controllers
+{
+    public class AcercasController : Controller
+    {
+        private gmaganaEntities db = new gmaganaEntities();
+
+        // GET: Acercas
+        public ActionResult Index()
+        {
+            return View(db.Acercas.ToList());
+        }
+
+        // GET: Acercas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // GET: Acercas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Acercas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Acerca1")] Acerca acerca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Acercas.Add(acerca);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(acerca);
+        }
+
+        // GET: Acercas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // POST: Acercas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Acerca1")] Acerca acerca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(acerca).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(acerca);
+        }
+
+        // GET: Acercas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // POST: Acercas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Acerca acerca = db.Acercas.Find(id);
+            db.Acercas.Remove(acerca);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Eportafolio1/Models/CurriculaController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eportafolio1.Models
+{
+    public class CurriculaController : Controller
+    {
+        private gmaganaEntities db = new gmaganaEntities();
+
+        // GET: Curricula
+        public ActionResult Index()
+        {
+            return View(db.Curricula.ToList());
+        }
+
+        // GET: Curricula/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curricula.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Curricula/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Area,linkedin,website,Phone,Email,Skills,Tools")] Curriculum curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Curricula.Add(curriculum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curricula.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // POST: Curricula/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Area,linkedin,website,Phone,Email,Skills,Tools")] Curriculum curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(curriculum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curricula.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // POST: Curricula/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Curriculum curriculum = db.Curricula.Find(id);
+            db.Curricula.Remove(curriculum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
